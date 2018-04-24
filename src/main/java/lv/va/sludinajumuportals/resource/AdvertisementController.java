59562c92e3ea21f69cdb1c27d42e2ab2f35612cb
Andris <andris.enins@accenture.com>
2018-04-24 16:07:22 +0300

    Add HTML and CSS

--- src/main/java/lv/va/sludinajumuportals/resource/AdvertisementController.java
+++ src/main/java/lv/va/sludinajumuportals/resource/AdvertisementController.java
@@ -3,7 +3,10 @@ package lv.va.sludinajumuportals.resource;
 import lv.va.sludinajumuportals.domain.Response;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class AdvertisementController {
@@ -20,5 +23,10 @@ public class AdvertisementController {
         return new Response("Hey Ya");
     }
 
+    @RequestMapping("/main")
+    public String getUI(Map<String, Object> model) {
+
+        return "main";
+    }
 
 }
