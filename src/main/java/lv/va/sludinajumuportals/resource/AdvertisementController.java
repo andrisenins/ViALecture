430050bd9b1ce1789c87929b5fc69f3672a7b68d
Andris <andris.enins@accenture.com>
2018-05-08 16:01:25 +0300

    Add db lecture 3

--- src/main/java/lv/va/sludinajumuportals/resource/AdvertisementController.java
+++ src/main/java/lv/va/sludinajumuportals/resource/AdvertisementController.java
@@ -7,11 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
+
+import static java.lang.Long.*;
 
 @Controller
 public class AdvertisementController {
@@ -54,6 +58,19 @@ public class AdvertisementController {
     public String advertisementCreationForm(Model model) {
         model.addAttribute("advertisement", new Advertisement());
         return "createform";
+    }
+
+    @PostMapping("/advertisement")
+    public String advertisementSubmit(Map<String, Object> model,
+                                      @ModelAttribute Advertisement advertisement) {
+        int nextAdvertisementId =
+                advertisementService.hardcodedAdvertisementList.size();
+        advertisement.setId(valueOf(nextAdvertisementId + 1));
+        advertisementService.hardcodedAdvertisementList
+                .add(0, advertisement);
+        model.put("advertisement", advertisement);
+        return "result";
+
     }
 
 
